4e75aa30d48707da4e3e1e5440a2b1cbec5b395d
Carl Engström <eng@spotify.com>
2016-08-22 20:33:07 +0200

    Use database and expose /cache endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:2
 
 RUN pip2 install flask
+RUN pip2 install psycopg2
 
 ADD app.py .
 
--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 from flask import request
+import psycopg2
+import sys
+
+con = psycopg2.connect(host='postgres', port='5432', database='postgres', user='postgres', password='secret')
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS reverse (input VARCHAR(255), output VARCHAR(255))")
+con.commit()
 
 app = Flask(__name__)
 @app.route("/reverse", methods = ["POST"])
 def reverse():
     reverse = request.get_data()[::-1]
+    cur = con.cursor()
+    cur.execute("INSERT INTO reverse VALUES ('" + request.get_data() + "', '" + reverse + "')")
+    con.commit()
     return reverse
+
+@app.route("/cache", methods = ["GET"])
+def get():
+    cur = con.cursor()
+    cur.execute('SELECT * FROM reverse')
+    return str(cur.fetchall())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
